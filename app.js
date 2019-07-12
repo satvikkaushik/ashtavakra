dab74d6a6fcfc98325f8eaeecd5795e33de93069
unknown <satvik71@gmail.com>
2019-07-12 20:58:00 +0530

    semifinal

--- app.js
+++ app.js
@@ -11,30 +11,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger("dev"));
 
-const MongoClient = require('mongodb').MongoClient;
 
-// replace the uri string with your connection string.
-const uri="mongodb+srv://admin2:raghavA3B3@cluster0-wuqbf.mongodb.net/test?retryWrites=true&w=majority"
-MongoClient.connect(uri,{ useNewUrlParser: true } ,function(err, client) {
-   if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-   }
-   console.log('Connected...');
-  //  const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   client.close();
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:ucusd-7@cluster0-wuqbf.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
-//express session middleware
-app.set('trust proxy', 1) // trust first proxy
-// app.use(session({
-//   secret: 'keyboard cat',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: true }
-// }));
-//Express Messages
-// app.use(require('connect-flash')());
+
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
@@ -52,6 +39,6 @@ app.use('/admin/products',adminProducts);
 
 
 
-app.listen(process.env.PORT, process.env.IP,function(){
-	console.log("Ashtavakra's server is working just fine!");
-});
+app.listen(process.env.PORT || 3003, () => {
+  console.log(“server started at port 3003”);
+  });
