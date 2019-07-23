1c20dcb44825ab3f0f8664e6a9600530b847e444
unknown <satvik71@gmail.com>
2019-07-23 15:01:01 +0530

    initial

--- app.js
+++ app.js
@@ -11,17 +11,27 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger("dev"));
 
+mongoose.connect('mongodb://localhost:27017/ashtavakra', {useNewUrlParser: true});
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:ucusd-7@cluster0-wuqbf.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = require(‘mongodb’).MongoClient;
+// const uri = "mongodb+srv://admin:raghava3b3@cluster0-wuqbf.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   client.close();
+// });
 
 
+//express session middleware
+app.set('trust proxy', 1) // trust first proxy
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { secure: true }
+// }));
+//Express Messages
+// app.use(require('connect-flash')());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
@@ -39,6 +49,8 @@ app.use('/admin/products',adminProducts);
 
 
 
-app.listen(process.env.PORT || 3003, () => {
-  console.log(“server started at port 3003”);
-  });
+// app.listen(3000,function(){
+// 	console.log("Ashtavakra's server is working just fine!");
+// });
+
+app.listen(process.env.PORT || 3000);
